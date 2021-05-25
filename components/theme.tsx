0b6ac2df830215945e1ce727cb4704934b1cdaf2
Logan Anderson <landerson2@upei.ca>
2021-05-25 10:14:23 -0300

    fixed theme

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { useForm, useFormScreenPlugin } from "tinacms";
 import { Theme_Document } from "../.tina/__generated__/types";
-import ThemeData from "../data/theme.json";
 
-export const ThemeContext = React.createContext(ThemeData);
+export const ThemeContext = React.createContext(null as any);
 
 const getInitialThemeMode = (_) => {
   if (typeof window !== "undefined") {
@@ -61,73 +59,6 @@ export const Theme: React.FC<{
     updateRenderColorMode(themeMode);
   }, [themeMode]);
 
-  // const [themeData, themeForm] = useForm({
-  //   initialValues: ThemeData,
-  //   label: "Theme",
-  //   fields: [
-  //     {
-  //       name: "color",
-  //       label: "Primary Color",
-  //       component: "select",
-  //       options: [
-  //         {
-  //           label: "Blue",
-  //           value: "blue",
-  //         },
-  //         {
-  //           label: "Teal",
-  //           value: "teal",
-  //         },
-  //         {
-  //           label: "Green",
-  //           value: "green",
-  //         },
-  //         {
-  //           label: "Red",
-  //           value: "red",
-  //         },
-  //         {
-  //           label: "Pink",
-  //           value: "pink",
-  //         },
-  //         {
-  //           label: "Purple",
-  //           value: "purple",
-  //         },
-  //         {
-  //           label: "Orange",
-  //           value: "orange",
-  //         },
-  //         {
-  //           label: "Yellow",
-  //           value: "yellow",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: "button.style",
-  //       label: "Button Style",
-  //       component: "select",
-  //       options: [
-  //         {
-  //           label: "Rounded Full",
-  //           value: "round",
-  //         },
-  //         {
-  //           label: "Rounded Partial",
-  //           value: "rounded",
-  //         },
-  //         {
-  //           label: "Square",
-  //           value: "square",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  //   onSubmit: (values) => {},
-  // });
-
-  // useFormScreenPlugin(themeForm);
   const themeData = theme.getThemeDocument.data;
   return (
     <ThemeContext.Provider value={{ themeMode, toggleThemeMode, ...themeData }}>
